59621bd5468861c9a672aec19d8a28003f8649c2
UtsavPreet <utsavpreet@gmail.com>
2018-03-21 22:46:22 +0530

    Completed the Changes and pushed to Github

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(document)
     .ready(function () {
-        bind('.mainContainer .useCaseContainer .useCase .buttonContainer .button', () => {
-            let text = $('.mainContainer .useCaseContainer .useCase .textBox')
+        let count = 0;
+        dummyNotification();
+        bind('.mainContainer .useCaseContainer .useCase .buttonContainer .button.modal', () => {
+            let text = $('.mainContainer .useCaseContainer .useCase .textBox.modal')
                 .val()
                 .trim();
             launchPopup(text);
@@ -13,16 +15,44 @@ $(document)
         })
         bind('.mainContainer .topBar .bell', () => {
             console.log('tapped');
-            let count = $('.count').text().trim();
-            $('.count').empty();
-            $('.notificationTab .header .count').append(count);
+            $('.mainContainer .topBar .count').text('');
+            rb('.mainContainer .notificationTab', 'notificationTemplate', notificationData);
+            notificationData.count = 0;
             $('.notificationTab').toggle();
-            
+            notificationData.notifications = [];
+        })
+        bind('.button.notification', () => {
+            let notificationText = $('.textBox.notification')
+                .val()
+                .trim();
+            notificationData.count++;
+            $('.mainContainer .topBar .count').text(notificationData.count);
+            if (notificationText !== '') {
+                notificationData
+                    .notifications
+                    .unshift({notificationText: notificationText});
+            } else {
+                notificationData
+                    .notifications
+                    .unshift({notificationText: 'Hello this is a dummy notification'});
+            }
+            console.log(notificationData);
+            rb('.mainContainer .notificationTab', 'notificationTemplate', notificationData);
         })
     })
 
-function launchPopup(text) {
+function launchPopup(content) {
     $('.overlay').show();
-    $('.modalContainer .modalContent').append(text);
+    $('.modalContainer .modalContent').append(content);
     $('.modalContainer').show();
+}
+function dummyNotification() {
+    notificationData.count++;
+    $('.mainContainer .topBar .count').text(notificationData.count);
+    notificationData
+        .notifications
+        .unshift({notificationText: 'Hello this is a dummy notification'});
+    console.log(notificationData);
+    rb('.mainContainer .notificationTab', 'notificationTemplate', notificationData);
+    setTimeout(dummyNotification,5000);
 }
